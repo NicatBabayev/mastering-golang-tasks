3ea836c54a3b36cd58970962dde54abec584c473
Babayev Nicat <52563250+NicatBabayev@users.noreply.github.com>
2024-09-25 15:33:54 +0400

    Second Session First Task (#2)
    * Second Session First Task
    
    * Second Session First Task
    
    * Make the `projectStatus` Uppercase
    
    Co-authored-by: Zaur Adigozalov <144166143+zaur-adigozalov@users.noreply.github.com>
    
    * Separate multi-line text into individual println statements
    
    Co-authored-by: Zaur Adigozalov <144166143+zaur-adigozalov@users.noreply.github.com>
    
    * Add required package.
    
    * Place const values in global scope
    
    ---------
    
    Co-authored-by: Zaur Adigozalov <144166143+zaur-adigozalov@users.noreply.github.com>

---
+++ session-2/TaskManagementSystem/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	low = iota
+	medium
+	high
+	tasksAvailable int = 100
+)
+
+func main() {
+	var (
+		projectStatus = "In Progress"
+		tasksCreated  = 25
+		isCompleted   = false
+	)
+
+	fmt.Println("Welcome to the Task Management System!")
+	fmt.Println("Project start date is: 2024-09-18 00:00:00")
+	fmt.Println("Project: Task Management System")
+
+	fmt.Println()
+
+	fmt.Printf("  Current project status: %s\n", strings.ToUpper(projectStatus))
+	fmt.Printf("  Tasks completed: %d out of %d\n", tasksCreated, tasksAvailable)
+	fmt.Printf("  Task priorities: %d-Low, %d-Medium, %d-High\n", low, medium, high)
+	fmt.Printf("  Is the project completed? %s\n", strconv.FormatBool(isCompleted))
+
+}
